b46cb7f2ad175e41843cea49823454932b8eff04
Lakkusya <67253763+Lakkusya@users.noreply.github.com>
2020-10-20 23:20:08 +0300

    Д/з 4

---
+++ js/task_4.js
@@ -0,0 +1,17 @@
+const hotel = {
+  name: 'Resort Hotel',
+  stars: 5,
+  capacity: 100,
+  showName() {
+    console.log(this.name);
+  },
+  changeCapacity(value) {
+    this.capacity = value;
+  },
+};
+
+hotel.showName(); // Resort Hotel
+
+hotel.changeCapacity(200);
+console.log(hotel.capacity); // 200
+
